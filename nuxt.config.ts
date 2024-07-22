72bfc9048e189711c335c914978ff339aa22d37e
Giacomo <giacomozr90@gmail.com>
2024-07-22 10:19:49 +0200

    (fix): add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
   },
   css: ["normalize.css/normalize.css", "~/assets/scss/main.scss"],
